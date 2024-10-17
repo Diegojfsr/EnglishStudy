64a35a02c40cf6bdf08e9bb107c0c10298a6c8a8
DiegoJfsr <diegojfsr@gmail.com>
2024-10-17 10:59:50 -0300

    variaveis

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ console.log("Hello World!")
 console.error("Este é um erro!")
 console.warn("Este é um aviso!")
 
+// variaveis
+
+var x = 10;
+let y = 15;
+const z = 20;
+
+if(1)
+{
+let y = 55;
+console.log(y);
+}
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+y = 25;
+console.log(y);
+
+
+
